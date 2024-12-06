5ca4a1dd42fc96e252d6bda51c78c3200f908efc
Pawan sharma <pawansh803@gmail.com>
2024-12-06 23:13:47 +0530

    theme

--- wp-content/themes/pbTheme/footer.php
+++ wp-content/themes/pbTheme/footer.php
@@ -1,9 +1,10 @@
-<footer>
-  <p><?php bloginfo('name'); ?> - &copy; <?php echo date('Y'); ?></p>
+<footer class="site-footer">
+    <p><?php bloginfo('name'); ?> - &copy; <?php echo date('Y'); ?></p>
 </footer>
 
 </div> <!-- container -->
 
 <?php wp_footer(); ?>
 </body>
+
 </html>
--- wp-content/themes/pbTheme/index.php
+++ wp-content/themes/pbTheme/index.php
@@ -4,14 +4,15 @@ get_header();
 
 if (have_posts()) :
     while (have_posts()) : the_post(); ?>
-    
-    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-    <?php the_content(); ?>
+        <article class="post">
+            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+            <?php the_content(); ?>
+        </article>
 
 <?php endwhile;
- else :
+else :
     echo "<p>No content found</p>";
- endif;
+endif;
 
 get_footer();
 
