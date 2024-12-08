749f84ce7f81e6c9c39e1312e2deac7f4caf547e
Pawan sharma <pawansh803@gmail.com>
2024-12-08 21:49:56 +0530

    theme

--- wp-content/themes/pbTheme/special-template.php
+++ wp-content/themes/pbTheme/special-template.php
@@ -1,11 +1,22 @@
 <?php
 
+/*
+Template Name: Special Layout
+*/
+
 get_header();
 
 if (have_posts()) :
     while (have_posts()) : the_post(); ?>
         <article class="post page">
             <h2><?php the_title(); ?></h2>
+
+            <!-- info-box -->
+             <div class="info-box">
+                <h4>Disclaimer Title</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi vel amet, facere commodi fugiat corporis tenetur nisi magnam ab deleniti, iste corrupti qui at.</p>
+             </div> <!-- /info-box -->
+
             <?php the_content(); ?>
         </article>
 
