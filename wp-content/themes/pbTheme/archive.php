52c0cf4f5784bc8b2bf110d40e774fcfa5f99391
Pawan sharma <pawansh803@gmail.com>
2024-12-15 16:51:26 +0530

    theme

--- wp-content/themes/pbTheme/archive.php
+++ wp-content/themes/pbTheme/archive.php
@@ -3,6 +3,30 @@
 get_header();
 
 if (have_posts()) :
+
+?>
+
+    <h2><?php
+
+        if (is_category()) {
+            echo 'This is a category';
+        } elseif (is_tag()) {
+            echo 'Tag';
+        } elseif (is_author()) {
+            echo 'Author';
+        } elseif (is_day()) {
+            echo 'Day archive';
+        } elseif (is_month()) {
+            echo 'Month';
+        } elseif (is_year()) {
+            echo 'Year';
+        } else {
+            echo 'Archive';
+        }
+
+        ?></h2>
+
+    <?php
     while (have_posts()) : the_post(); ?>
         <article class="post">
             <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
