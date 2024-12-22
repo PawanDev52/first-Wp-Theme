c815d5270a74df7162b8131beea5bd59b3800ef6
Pawan sharma <pawansh803@gmail.com>
2024-12-22 21:05:26 +0530

    theme

--- wp-content/themes/pbTheme/functions.php
+++ wp-content/themes/pbTheme/functions.php
@@ -35,4 +35,12 @@ function has_children()
     return count($pages);
 }
 
+// Customize excerpt word count length
+function custom_excerpt_length()
+{
+    return 25;
+}
+
+add_filter('excerpt_length', 'custom_excerpt_length');
+
 ?>
